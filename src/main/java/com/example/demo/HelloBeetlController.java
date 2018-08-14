a28d68e5afcd8ad147b3777d15a2ac65e0d04912
Ray Rui <Ray.Rui@lombardrisk.com>
2018-08-14 14:55:14 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/HelloBeetlController.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/home")
+public class HelloBeetlController {
+
+    private static Logger logger = LoggerFactory.getLogger(HelloBeetlController.class);
+
+    /**
+     * 测试beetl模板
+     *
+     * @return
+     */
+    @RequestMapping("/add")
+    public ModelAndView home() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        logger.info("add request");
+        modelAndView.addObject("email", "apk2sf@163.com");
+        modelAndView.setViewName("index");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/house_list")
+    public ModelAndView list() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        logger.info("add request");
+        modelAndView.addObject("email", "apk2sf@163.com");
+        modelAndView.setViewName("house_list");
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/loupanchart")
+    public ModelAndView loupanchart() {
+
+        ModelAndView modelAndView = new ModelAndView();
+        logger.info("add request");
+        modelAndView.addObject("email", "apk2sf@163.com");
+        modelAndView.setViewName("loupanchart");
+
+        return modelAndView;
+    }
+
+
+}
